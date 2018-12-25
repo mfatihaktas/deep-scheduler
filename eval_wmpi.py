9ac02ff745c89fd177429edb8f12034446efdf4d
Mehmet Fatih Aktas <mfatihaktas@gmail.com>
2018-12-25 17:24:22 -0500

    Fixed eval_wmpi.py

--- eval_wmpi.py
+++ eval_wmpi.py
@@ -1,9 +1,9 @@
-import sys
+import sys, time
 import numpy as np
 from mpi4py import MPI
 
-from rvs import *
 from scheduler import *
+from modeling import *
 
 def eval_wmpi(rank):
   log(INFO, "starting;", rank=rank)
@@ -17,6 +17,9 @@ def eval_wmpi(rank):
     if rank == 0:
       scheri__Esl_l_l = []
       for i, scher in enumerate(scher_l):
+        log(INFO, "Master will send for scher_i= {}".format(i) )
+        sys.stdout.flush()
+        
         for p in range(1, num_mpiprocs):
           scher_i = np.array([i], dtype='i')
           comm.Send([scher_i, MPI.INT], dest=p)
@@ -26,32 +29,39 @@ def eval_wmpi(rank):
           Esl = np.empty(1, dtype=np.float64)
           comm.Recv(Esl, source=p)
           Esl_l.append(Esl)
-        log(INFO, "\nEval;", scher=scher, Esl_l=Esl_l)
+        log(INFO, "Master; ro= {}".format(ro), scher=scher, Esl_l=Esl_l)
         sys.stdout.flush()
         scheri__Esl_l_l.append(Esl_l)
       ro__scheri__Esl_l_l_m[ro] = scheri__Esl_l_l
+      time.sleep(2)
         
-      # for p in range(1, num_mpiprocs):
-      #   scher_i = np.array([-1], dtype='i')
-      #   comm.Send([scher_i, MPI.INT], dest=p)
-      #   print("Sent req scher_i= {} to p= {}".format(scher_i, p) )
-      blog(scher_l=scher_l, ro__scheri__Esl_l_l_m=ro__scheri__Esl_l_l_m)
+      for p in range(1, num_mpiprocs):
+        scher_i = np.array([-1], dtype='i')
+        comm.Send([scher_i, MPI.INT], dest=p)
+        print("Sent req scher_i= {} to p= {}".format(scher_i, p) )
     else:
-      # while True:
-      scher_i = np.empty(1, dtype='i')
-      comm.Recv([scher_i, MPI.INT], source=0)
-      scher_i = scher_i[0]
-      # if scher_i == -1:
-      #   return
-      
-      scher = scher_l[scher_i]
-      if scher_i == 0:
-        scher.restore(ro__learning_count_m[ro] )
-      t_s_l, t_a_l, t_r_l, t_sl_l, load_mean, droprate_mean = sample_traj(sinfo_m, scher, use_lessreal_sim)
-      
-      Esl = np.array([np.mean(t_sl_l) ], dtype=np.float64)
-      comm.Send([Esl, MPI.FLOAT], dest=0)
+      log(INFO, "rank= {} waiting for Master".format(rank) )
       sys.stdout.flush()
+      while True:
+        scher_i = np.empty(1, dtype='i')
+        comm.Recv([scher_i, MPI.INT], source=0)
+        scher_i = scher_i[0]
+        if scher_i == -1:
+          break
+        
+        scher = scher_l[scher_i]
+        # if scher_i == 0:
+        #   scher.restore(ro__learning_count_m[ro] )
+        log(INFO, "rank= {} will sim with scher= {}".format(rank, scher) )
+        sys.stdout.flush()
+        t_s_l, t_a_l, t_r_l, t_sl_l, load_mean, droprate_mean = sample_traj(sinfo_m, scher, lessreal_sim)
+        log(INFO, "rank= {}".format(rank), load_mean=load_mean, scher=scher)
+        
+        Esl = np.array([np.mean(t_sl_l) ], dtype=np.float64)
+        comm.Send([Esl, MPI.FLOAT], dest=0)
+        sys.stdout.flush()
+  if rank == 0:
+    blog(scher_l=scher_l, ro__scheri__Esl_l_l_m=ro__scheri__Esl_l_l_m)
 
 if __name__ == "__main__":
   comm = MPI.COMM_WORLD
@@ -63,21 +73,21 @@ if __name__ == "__main__":
   R = Uniform(1, 1)
   M = 1000
   sching_m = {
-    'a': 3, 'N': -1,
+    'a': 5, 'N': -1,
     'learner': 'QLearner_wTargetNet_wExpReplay',
     'exp_buffer_size': 100*M, 'exp_batch_size': M}
   mapping_m = {'type': 'spreading'}
   
-  use_lessreal_sim = True
-  log(INFO, "use_lessreal_sim= {}".format(use_lessreal_sim) )
-  if use_lessreal_sim:
-    b, beta = 10, 4
+  lessreal_sim = True
+  log(INFO, "lessreal_sim= {}".format(lessreal_sim) )
+  if lessreal_sim:
+    b, beta = 10, 2
     L = Pareto(b, beta)
     a, alpha = 1, 3
     Sl = Pareto(a, alpha)
     
     sinfo_m = {
-      'njob': 2000*N, # 10*N,
+      'njob': 10*N, # 3000*N,
       'nworker': N, 'wcap': Cap, 'ar': None,
       'k_rv': k, 'reqed_rv': R, 'lifetime_rv': L,
       'straggle_m': {'slowdown': lambda load: Sl.sample() } }
@@ -102,10 +112,11 @@ if __name__ == "__main__":
     0.75: 520,
     0.85: None}
   
-  ro_l = [0.3, 0.5, 0.6, 0.75, 0.85]
-  scher = RLScher(sinfo_m, mapping_m, sching_m, save_dir='save_expreplay_persist')
+  # ro_l = [0.3, 0.5, 0.6, 0.75, 0.85]
+  ro_l = [0.75, 0.85]
+  # scher = RLScher(sinfo_m, mapping_m, sching_m, save_dir='save_expreplay_persist')
   scher_l = [
-    scher, 
+    # scher, 
     Scher(mapping_m, {'type': 'plain', 'a': 0} ),
     Scher(mapping_m, {'type': 'plain', 'a': sching_m['a'] } ) ]
   # {'type': 'expand_if_totaldemand_leq', 'threshold': 100, 'a': 1}
--- jscript.sh
+++ jscript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --partition=main             # Partition (job queue)
-#SBATCH --job-name=learn_wmpi
+#SBATCH --job-name=eval_wmpi
 #SBATCH --nodes=11              # Number of nodes you require
 #SBATCH --ntasks=11             # Total # of tasks across all nodes
 #SBATCH --cpus-per-task=1            # Cores per task (>1 if multithread tasks)
@@ -10,5 +10,5 @@
 #SBATCH --output=log/slurm.%N.%j.out
 
 export MV2_ENABLE_AFFINITY=0
-srun --mpi=pmi2 python3 /home/mfa51/deep-scheduler/learn_wmpi.py
+srun --mpi=pmi2 python3 /home/mfa51/deep-scheduler/eval_wmpi.py
   
